cfeda13b49aadb54fed957ca0198a806a561c5f9
0BVer <dr0joon@gmail.com>
2022-02-25 23:35:31 +0900

    FEATURE : add css

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from polls.models import Question
+from polls.models import Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
